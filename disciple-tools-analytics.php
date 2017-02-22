a48bef4f4ae8cad8ba081dfa9ff7420a2697f6f0
Chris Wynn <chrisw@MacBook-Pro-2.local>
2017-02-22 12:10:11 -0700

    Update Constants

--- disciple-tools-analytics.php
+++ disciple-tools-analytics.php
@@ -1,9 +1,8 @@
 <?php
-
 /**
- * Plugin Name: Disciple Tools Google Analytics
+ * Plugin Name: Disciple Tools Analytics
  * Plugin URI:
- * Description: Disciple Tools Google Analytics Integration
+ * Description: Disciple Tools -  Google Analytics Integration
  *
  * Adapted from Google Analytics Plugin by Sharethis
  * Original Plugin URI: http://wordpress.org/extend/plugins/googleanalytics/
@@ -13,6 +12,8 @@
  *
  *
  */
+
+
 if ( !defined( 'WP_CONTENT_URL' ) ) {
 	define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
 }
@@ -29,10 +30,10 @@ if ( !defined( 'GA_NAME' ) ) {
 	define( 'GA_NAME', 'googleanalytics' );
 }
 if ( !defined( 'GA_PLUGIN_DIR' ) ) {
-	define( 'GA_PLUGIN_DIR', WP_PLUGIN_DIR . '/disciple-tools-analytics' );
+	define( 'GA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
 if ( !defined( 'GA_PLUGIN_URL' ) ) {
-	define( 'GA_PLUGIN_URL', WP_PLUGIN_URL . '/disciple-tools-analytics' );
+	define( 'GA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 if ( !defined( 'GA_MAIN_FILE_PATH' ) ) {
 	define( 'GA_MAIN_FILE_PATH', __FILE__ );
@@ -42,8 +43,8 @@ if ( !defined( 'GA_SHARETHIS_SCRIPTS_INCLUDED' ) ) {
 }
 
 define( 'GOOGLEANALYTICS_VERSION', '2.1.1' );
-include_once GA_PLUGIN_DIR . '/overwrite/ga_overwrite.php';
-include_once GA_PLUGIN_DIR . '/class/Ga_Autoloader.php';
+include_once GA_PLUGIN_DIR . 'overwrite/ga_overwrite.php';
+include_once GA_PLUGIN_DIR . 'class/Ga_Autoloader.php';
 
 Ga_Autoloader::register();
 Ga_Hook::add_hooks( GA_MAIN_FILE_PATH );
